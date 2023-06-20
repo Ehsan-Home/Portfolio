04674a8ae87f2889426e90eb4b5044d4feafaf22
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-20 07:24:41 -0700

    antd installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Button } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>EhsanGhasaei</div>;
+  return (
+    <>
+      <Button type="primary">Click me</Button>
+    </>
+  );
 }
