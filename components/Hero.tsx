6e6982fb0e7148f4504e1f460e190daf2c1d4343
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-20 10:08:48 -0700

    profile pic size adjusted, and hero style added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,22 @@
 import { Col, Row, Typography } from "antd";
 import styles from "@/styles/hero.module.css";
 import Image from "next/image";
+import profilePic from "@/public/images/profile.jpeg";
 
 const { Title, Paragraph } = Typography;
 
 const Hero = () => {
   return (
     <Row gutter={8} align="middle" className={styles.heroHeight}>
-      <Col span={18}>
+      <Col lg={18} md={18} sm={24}>
         <Title level={2}>Hey, I&apos;m Ehsan 👋🏻</Title>
         <Paragraph type="secondary">
           Welcome to my page! I&apos;m a software engineer, and I usually code
           in React and Python.
         </Paragraph>
       </Col>
-      <Col span={6}>
-        <Image
-          src="/images/profile.jpeg"
-          width={200}
-          height={200}
-          alt="profile-image"
-        />
+      <Col lg={6} md={6} sm={24}>
+        <Image className={styles.image} src={profilePic} alt="profile-image" />
       </Col>
     </Row>
   );
