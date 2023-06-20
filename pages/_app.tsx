592847d9d89ce0ce7c44f9177cfd662fe66add1c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-20 08:04:12 -0700

    skeleton moved to _app.tsx for reusability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Layout } from "antd";
+import Navbar from "@/components/Navbar";
+
+const { Header, Content, Footer } = Layout;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,7 +12,13 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Ehsan Ghasaei</title>
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Navbar />
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,5 @@ import { Button, Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
 export default function Home() {
-  return (
-    <Layout>
-      <Navbar />
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
-    </Layout>
-  );
+  return <>content</>;
 }
