0eb52acd5f147294ca709948d5b18701d6953365
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-22 11:20:47 -0700

    title of card is word break

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,26 +1,26 @@
 import { Project } from "@/models/ProjectModel";
 import { Card, Space, Tag, Typography } from "antd";
-import Image from "next/image";
 import Link from "next/link";
-import langs from "@/utils/languages.json";
+import styles from "@/styles/projectCard.module.css";
 
 const { Title } = Typography;
 
 const ProjectCard = ({ project }: { project: Project }) => {
-  console.log({ langs });
-  const { Python } = langs;
-  console.log({ Python });
-
   return (
     <Card
-      cover={null}
+      // style={{ height: "100%", width: "100%" }}
+      type="inner"
       actions={[
         <Link href={project.link} key={project.link}>
           Link
         </Link>,
       ]}
+      title={
+        <Title level={5} className={styles.wrap}>
+          {project.title}
+        </Title>
+      }
     >
-      <Title level={5}>{project.title}</Title>
       <Space wrap direction="horizontal">
         {project.techs.map((tech) => (
           <Tag key={tech.name} color={tech.color}>
