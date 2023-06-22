a7d93e81b0a2b8da2c34b99f84f4792dced104a4
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-22 16:23:26 -0700

    meta tag added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Ehsan Ghasaei</title>
+        <meta name="description" content="Ehsan Ghasaei Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
         {/* <Navbar /> */}
