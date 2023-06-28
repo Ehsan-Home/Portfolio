3ca5469bda9811aea17cd1adce717bec4f3936b5
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-27 18:08:01 -0700

    Readme card top margin added

--- components/Readme.tsx
+++ components/Readme.tsx
@@ -1,9 +1,10 @@
 import { Card } from "antd";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import styles from "@/styles/readme.module.css";
 
 const Readme = ({ children }: { children: string }) => {
   return (
-    <Card>
+    <Card className={styles["mt-50"]}>
       <ReactMarkdown>{children}</ReactMarkdown>
     </Card>
   );
