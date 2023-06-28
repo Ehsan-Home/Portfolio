df5a0cf7722952c2ce123e6591cdf465f9cb507f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-27 18:52:15 -0700

    Back button added to README

--- components/Readme.tsx
+++ components/Readme.tsx
@@ -1,11 +1,14 @@
-import { Card } from "antd";
+import { Card, Divider } from "antd";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import styles from "@/styles/readme.module.css";
+import Link from "next/link";
 
 const Readme = ({ children }: { children: string }) => {
   return (
     <Card className={styles["mt-50"]}>
       <ReactMarkdown>{children}</ReactMarkdown>
+      <Divider />
+      <Link href="/">Back to home</Link>
     </Card>
   );
 };
